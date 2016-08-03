7b4b82f3a1f577979c8a0c22ee207912f1b47162
akshitbrightlifecare <akshit.sawhney@brightlifecare.com>
2016-08-03 11:36:44 +0530

    Different timeout files
    Different timeout files

---
+++ app7.js
@@ -0,0 +1,11 @@
+setTimeout(function() {
+    $.getJSON("myJSON.js", function(result){
+        var cList = $("#myID");
+        $.each(result, function(i)
+        {
+            var li = $('<li/>')
+                .text(result[i].name)
+                .appendTo(cList);
+        });
+    });
+},7000);
