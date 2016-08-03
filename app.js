97a2d1507f3d13c16267e25c896f4c283bb5f928
healthkarttesting <healthkarttesting@gmail.com>
2016-08-03 11:13:01 +0530

    Applying timeout
    Applying timeout

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-$.getJSON("myJSON.js", function(result){
-    var cList = $("#myID");
-    $.each(result, function(i)
-    {
-        var li = $('<li/>')
-            .text(result[i].name)
-            .appendTo(cList);
+setTimeout(function() {
+    $.getJSON("myJSON.js", function(result){
+        var cList = $("#myID");
+        $.each(result, function(i)
+        {
+            var li = $('<li/>')
+                .text(result[i].name)
+                .appendTo(cList);
+        });
     });
-});
+},5000);
