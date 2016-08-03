16be23a258ca0001b78654dfe2b383ef0b89e47d
akshitbrightlifecare <akshit.sawhney@brightlifecare.com>
2016-08-03 11:54:39 +0530

    making 5 seconds delay page heavy(ready case)
    making 5 seconds delay page heavy

---
+++ app5a.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    setTimeout(function() {
+        $.getJSON("myJSON.js", function(result){
+            var cList = $("#myID");
+            $.each(result, function(i)
+            {
+                var li = $('<li/>')
+                    .text(result[i].name)
+                    .appendTo(cList);
+            });
+        });
+    },5000);
+});
